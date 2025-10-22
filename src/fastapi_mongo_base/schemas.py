7ef07dad18287cbc345232710faac6e2ed58fa4e
Mahdi Kiani <mahdikiany@gmail.com>
2025-10-22 09:28:57 +0300

    Refactor PaginatedResponse class in schemas.py to reintroduce generic type parameter for enhanced type safety and flexibility.

--- src/fastapi_mongo_base/schemas.py
+++ src/fastapi_mongo_base/schemas.py
@@ -99,7 +99,7 @@ class TenantUserEntitySchema(TenantScopedEntitySchema, UserOwnedEntitySchema):
 TSCHEMA = TypeVar("TSCHEMA", bound=BaseModel)
 
 
-class PaginatedResponse(BaseModel):
+class PaginatedResponse[TSCHEMA: BaseModel](BaseModel):
     heads: dict[str, dict[str, str]] = Field(default_factory=dict)
     items: list[TSCHEMA]
     total: int
